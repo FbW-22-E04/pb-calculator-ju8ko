efbcef4bd3e8184e67abdb8a7d5c06c612331258
ju8ko <julia.x.kobzar@gmail.com>
2022-09-07 10:31:35 +0200

    Exercise is done

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
+class Calculator {
+  constructor() {
+    //properties
+    this.PI = Math.PI;
+    this.E = Math.E;
+  }
 
-class Calculator{
-    constructor(){
-        //properties
-    }
+  //getter
+  get pi() {
+    return `PI (${this.PI})`;
+  }
+  get e() {
+    return `Eulner's number (${this.E})`;
+  }
 
-    //getter
-    
-    //methods
+  //methods
+  ratio(x, y, width) {
+    return (width / y) * x;
+  }
+
+  percentage(x, y) {
+    return (100 * x) / y;
+  }
+
+  add(x, y) {
+    return x + y;
+  }
+
+  subtract(x, y) {
+    return x - y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+
+  divide(x, y) {
+    y === 0 ? console.log("error") : console.log(x / y);
+  }
+
+  modulation(x, y) {
+    y === 0 ? console.log("error") : console.log(x % y);
+  }
+
+  elevate(x, y) {
+    return Math.pow(x, y);
+  }
+
+  sqrt(x) {
+    return Math.sqrt(x);
+  }
 }
+const calculate = new Calculator();
+
+console.log(calculate.pi); //PI (3.141592653589793)
+console.log(calculate.e); //Eulner's number (2.718281828459045)
